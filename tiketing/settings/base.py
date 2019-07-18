7f426457405cfb03107cb12f846a3c87c8c65380
Anderi Setiawan <anderi.setiawan@gmail.com>
2019-07-18 13:37:54 +0700

    Update static path

--- tiketing/settings/base.py
+++ tiketing/settings/base.py
@@ -113,6 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'env_root', 'static_root')
 
 
 KAI_BASE_API = config('KAI_BASE_API')
